1ca8e14928508cd9082405cede778e6bf319ae06
StarmanKoda <78097086+StarmanKoda@users.noreply.github.com>
2021-01-27 11:33:26 -0500

    J
    j

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float Speed;
+    public int Health;
+    public GameObject Bob;
+    public KeyCode Right;
+    public KeyCode Left;
+    public KeyCode Jump;
+    public Rigidbody RB;
+    public float JumpForce;
+    public bool IsGrounded;
+    public float TargetDistance;
+    
+
+
+// Update is called once per frame
+public void Update()
+    {
+        
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(0, -1, 0), Color.red);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, TargetDistance))
+        {
+            IsGrounded = true;
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            IsGrounded = false;
+            Debug.Log("No Hit");
+        }
+
+        if (Input.GetKey(Right))
+        {
+            Bob.transform.Translate(new Vector2(Speed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(Left))
+        {
+            Bob.transform.Translate(new Vector2(-Speed * Time.deltaTime, 0));
+        }
+        
+
+        if (IsGrounded && Input.GetKey(Jump))
+        {
+            RB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+
+private void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
